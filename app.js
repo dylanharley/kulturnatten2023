7b8600a00b79d290a606ba4632038c528a6b864d
Aschie4589 <tommaso.aschieri@gmail.com>
2023-09-25 14:22:02 +0200

    Resized p5.js div

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ myCanvas.parent("quantumDiv");
 
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
   }
   
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+function draw() {
+if (mouseIsPressed) {
+    fill(0);
+} else {
+    fill(255);
+}
+ellipse(mouseX, mouseY, 80, 80);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+ }
