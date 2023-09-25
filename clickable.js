0bb848bf20b4113deb18ee3b910f0efddf9ea73b
Aschie4589 <tommaso.aschieri@gmail.com>
2023-09-25 14:42:08 +0200

    Added "Clickable" class. Added "GUI" class.

---
+++ clickable.js
@@ -0,0 +1,12 @@
+class Clickable {
+    constructor(onclick){
+        this.onclick = onclick
+    }
+    
+    draw(){
+    } 
+
+    ismouseover(){
+    }
+
+}
--- gui.js
+++ gui.js
@@ -1,1 +1,6 @@
-console.log("GUI!")
+class GUI {
+    constructor(canvas){
+        this.canvas = canvas
+    }
+
+}
