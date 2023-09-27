21603a7507e2fcd58e6c7ee8195498f8e048fe05
dylanharley <dylan.harley@gmail.com>
2023-09-27 11:06:51 +0200

    Fixed behaviour of q gates under window resizing

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const s = (sketch)=>{
 
     sketch.setup = ()=>{
         var myCanvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-        myCanvas.parent("quantumDiv");
-
-        Xgate = new QuantumGate("Vend", 150, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
-        Igate = new QuantumGate("Vend ikke", 300, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
-        Hgate = new QuantumGate("Superposition", 450, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
+        myCanvas.parent("quantumDiv");        
         console.log(sketch)
         gui = new GUI(sketch)
+        Xgate = new QuantumGate("Vend", gui.gatesPanelPaddingLeft+100, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
+        Igate = new QuantumGate("Vend ikke", gui.gatesPanelPaddingLeft + 0.5*gui.gatesPanelWidth, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
+        Hgate = new QuantumGate("Superposition", gui.gatesPanelPaddingLeft + gui.gatesPanelWidth-100, sketch.windowHeight-100,100,100, ()=>{console.log("hi")})
     }
     
     sketch.draw = ()=>{
@@ -35,6 +34,9 @@ const s = (sketch)=>{
     sketch.windowResized = ()=>{
         sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
         gui.resize()
+        Xgate.set_position(gui.gatesPanelPaddingLeft+100,sketch.windowHeight-100);
+        Igate.set_position(gui.gatesPanelPaddingLeft + 0.5*gui.gatesPanelWidth,sketch.windowHeight-100);
+        Hgate.set_position(gui.gatesPanelPaddingLeft + gui.gatesPanelWidth-100,sketch.windowHeight-100);
     }
 
     sketch.mouseClicked = ()=>{
